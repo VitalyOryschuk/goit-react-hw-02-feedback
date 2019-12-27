09b38f703ea2ab6ea270700de8a26b18cced0671
Vitaly Oryschuk <Leilyua@ukr.net>
2019-12-27 16:38:26 +0200

    Updates

---
+++ precache-manifest.11aa4c8a9974ab2ff734e2d63ef3f11e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82aac127ec7a60bbcb042c98be0d7ad3",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "fadd8a0a8767820c8cc5",
+    "url": "/goit-react-hw-02-feedback/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "0a84219314def9925994",
+    "url": "/goit-react-hw-02-feedback/static/css/main.ff1bf26c.chunk.css"
+  },
+  {
+    "revision": "fadd8a0a8767820c8cc5",
+    "url": "/goit-react-hw-02-feedback/static/js/2.994b25dd.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/goit-react-hw-02-feedback/static/js/2.994b25dd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0a84219314def9925994",
+    "url": "/goit-react-hw-02-feedback/static/js/main.118ac5ca.chunk.js"
+  },
+  {
+    "revision": "6d4883b48959809198b2",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.f22f9b33.js"
+  }
+]);
